5f223a067a7ebe8e557afe910639fca0364addbd
elase huang <elase.huang@outlook.com>
2023-10-27 15:39:28 +0800

    chore: spport non-root user run

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ echo "$ext"
 if [[ $ext -ne 0 ]]; then
   exit $ext
 fi
+sudo setcap cap_net_admin=eip target/release/tcp_rust
 target/release/tcp_rust &
 pid=$!
-ip addr add 192.168.0.1/24 dev tun0
-ip link set up dev tun0
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
 trap "kill $pid" INT TERM
 wait $pid
